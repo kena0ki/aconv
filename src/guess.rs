4b5ed4e756315ff711bffa200e5f78bf52c30396
kena0ki <ken3574.25@gmail.com>
2021-10-11 21:46:11 +0900

    test

--- src/guess.rs
+++ src/guess.rs
@@ -8,6 +8,10 @@ pub struct GuessResult {
 }
 
 pub fn guess(src: &mut [u8], eof: bool, num_non_aschii: usize) -> GuessResult {
+
+    // BOM sniffing
+
+    // guess BOMless encodings
     let num_read = src.len();
     let mut det = cd::EncodingDetector::new();
     let mut aschii_cnt = 0;
@@ -32,3 +36,57 @@ pub fn guess(src: &mut [u8], eof: bool, num_non_aschii: usize) -> GuessResult {
         eof: eof && exhausted,
     };
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn guess_test() {
+        // utf16
+        let mut hoge = super::cd::EncodingDetector::new();
+        // let string = b"\x72\xac\x30\x68\x73\x2b"; // 犬と猫 in UTF-16
+        // let string = b"\xfe\xff\x30\x88\x30\x46\x30\x53\x30\x5d"; // ようこそ in UTF-16
+        let string = b"\xfe\xff\x30\x88\x30\x46\x30\x53\x30\x5d\x00\x0a"; // ようこそ in UTF-16 without BOM
+        hoge.feed(string, false);
+        let (fuga, success) = hoge.guess_assess(None, true);
+        println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
+
+        // utf16 BOMless
+        let mut hoge = super::cd::EncodingDetector::new();
+        let string = b"\x30\x88\x30\x46\x30\x53\x30\x5d"; // ようこそ in UTF-16 without BOM
+        hoge.feed(string, false);
+        let (fuga, success) = hoge.guess_assess(None, true);
+        println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
+
+        // utf8
+        let mut hoge = super::cd::EncodingDetector::new();
+        // let string = "こんにちは".as_bytes(); // こんにちは in UTF-8
+        // let string = b"\xe3\x82\x88\xe3\x81\x86\xe3\x81\x93\xe3\x81\x9d"; // ようこそ in UTF-8
+        let string = "よ".as_bytes(); // ようこそ in UTF-8
+        hoge.feed(string, false);
+        let (fuga, success) = hoge.guess_assess(None, true);
+        println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
+
+        // sjis
+        let string = b"\x82\xe6\x82\xa8\x82\xb1\x82\xbb"; // ようこそ in Shift-JIS
+        let mut hoge = super::cd::EncodingDetector::new();
+        hoge.feed(string, false);
+        let (fuga, success) = hoge.guess_assess(None, true);
+        println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
+
+        // eucjp
+        let string = b"\xa4\xe8\xa4\xa6\xa4\xb3\xa4\xbd"; // ようこそ in eucjp
+        let mut hoge = super::cd::EncodingDetector::new();
+        hoge.feed(string, false);
+        let (fuga, success) = hoge.guess_assess(None, true);
+        println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
+
+        // binary data
+        let string = b"\x00\xe8\xa4\x00\xa4\x00\xa4\x00"; // random bynary data
+        let mut hoge = super::cd::EncodingDetector::new();
+        hoge.feed(string, false);
+        let (fuga, success) = hoge.guess_assess(None, true);
+        println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ pub mod constants;
 pub mod transcoder;
 pub mod guess;
 
-use chardetng as cd;
 use encoding_rs as enc;
 use std::io;
 use std::fs;
@@ -158,51 +157,3 @@ mod tests {
     }
 }
 
-
-fn _guess_test() {
-    // utf16
-    let mut hoge = cd::EncodingDetector::new();
-    // let string = b"\x72\xac\x30\x68\x73\x2b"; // 犬と猫 in UTF-16
-    // let string = b"\xfe\xff\x30\x88\x30\x46\x30\x53\x30\x5d"; // ようこそ in UTF-16
-    let string = b"\xfe\xff\x30\x88\x30\x46\x30\x53\x30\x5d\x00\x0a"; // ようこそ in UTF-16 without BOM
-    hoge.feed(string, false);
-    let (fuga, success) = hoge.guess_assess(None, true);
-    println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
-
-    // utf16 BOMless
-    let mut hoge = cd::EncodingDetector::new();
-    let string = b"\x30\x88\x30\x46\x30\x53\x30\x5d"; // ようこそ in UTF-16 without BOM
-    hoge.feed(string, false);
-    let (fuga, success) = hoge.guess_assess(None, true);
-    println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
-
-    // utf8
-    let mut hoge = cd::EncodingDetector::new();
-    // let string = "こんにちは".as_bytes(); // こんにちは in UTF-8
-    // let string = b"\xe3\x82\x88\xe3\x81\x86\xe3\x81\x93\xe3\x81\x9d"; // ようこそ in UTF-8
-    let string = "よ".as_bytes(); // ようこそ in UTF-8
-    hoge.feed(string, false);
-    let (fuga, success) = hoge.guess_assess(None, true);
-    println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
-
-    // sjis
-    let string = b"\x82\xe6\x82\xa8\x82\xb1\x82\xbb"; // ようこそ in Shift-JIS
-    let mut hoge = cd::EncodingDetector::new();
-    hoge.feed(string, false);
-    let (fuga, success) = hoge.guess_assess(None, true);
-    println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
-
-    // eucjp
-    let string = b"\xa4\xe8\xa4\xa6\xa4\xb3\xa4\xbd"; // ようこそ in eucjp
-    let mut hoge = cd::EncodingDetector::new();
-    hoge.feed(string, false);
-    let (fuga, success) = hoge.guess_assess(None, true);
-    println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
-
-    // binary data
-    let string = b"\x00\xe8\xa4\x00\xa4\x00\xa4\x00"; // random bynary data
-    let mut hoge = cd::EncodingDetector::new();
-    hoge.feed(string, false);
-    let (fuga, success) = hoge.guess_assess(None, true);
-    println!("{:?}, {:?}, {:?}", success, fuga, fuga.decode(string));
-}
