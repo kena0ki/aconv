264882fc68a8c508e9d1e59834c2951efdcf8e11
kena0ki <ken3574.25@gmail.com>
2021-10-16 18:46:54 +0900

    test lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,8 @@ pub fn cli(opt: option::Opt) {
     transcode(&mut file, &mut stdout, to_code, input_buffer, output_buffer, opt);
 }
 
-pub fn transcode(read: &mut impl io::Read, write: &mut impl io::Write, encoding: &'static enc::Encoding, input_buffer: &mut [u8], output_buffer: &mut [u8], opt: option::Opt) 
+pub fn transcode(read: &mut impl io::Read, write: &mut impl io::Write, encoding: &'static enc::Encoding,
+    input_buffer: &mut [u8], output_buffer: &mut [u8], opt: option::Opt)
     -> Option<&'static enc::Encoding> {
 
     // guess the input encoding using up to a few Kbytes of byte sequences
@@ -47,6 +48,12 @@ pub fn transcode(read: &mut impl io::Read, write: &mut impl io::Write, encoding:
                     try_write(|| write.write_all(&buf_guess));
                     try_write(|| io::copy(read, write).map(|_| ()));
                     return transcoder.src_encoding();
+                }
+                if transcoder.dst_encoding() == enc::UTF_16BE && [0xFE,0xFF] != output_buffer[..2] {
+                    try_write(|| write.write_all(b"\xFE\xFF")); // add a BOM
+                }
+                if transcoder.dst_encoding() == enc::UTF_16LE && [0xFF,0xFE] != output_buffer[..2] {
+                    try_write(|| write.write_all(b"\xFF\xFE")); // add a BOM
                 }
                 // write transcoded bytes in buffer
                 try_write(|| write.write_all(&output_buffer[..num_written]));
@@ -137,7 +144,7 @@ mod tests {
                 // let input_buffer = &mut [0u8; 32]; // 5K bytes
                 let output_buffer = &mut [0u8; 10*1024]; // 10K bytes
                 // let output_buffer = &mut [0u8; 128]; // 10K bytes
-                super::transcode(ifile_handle, output, enc.unwrap(), input_buffer, output_buffer, opt).unwrap();
+                super::transcode(ifile_handle, output, enc.unwrap(), input_buffer, output_buffer, opt);
                 let test_data = path::Path::new("test_data");
                 let efile_handle = &mut std::fs::File::open(test_data.join($expected_file)).unwrap();
                 let expected_string = &mut Vec::with_capacity(20*1024);
@@ -162,6 +169,15 @@ mod tests {
     test_transcode!(transcode_koi8r_utf8       , "koi8-r_ru.txt"       , "utf8_ru.txt"     , "utf8");
     test_transcode!(transcode_windows1252_utf8 , "windows-1252_es.txt" , "utf8_es.txt"     , "utf8");
 
-    // test_transcode!(transcode_sjis_utf82        , "sjis_ja2.txt"         , "utf8_ja2.txt"     , "utf8");
+    test_transcode!(transcode_utf8_utf16le     , "utf8_th.txt"     , "utf16le_BOM_th.txt"  , "utf-16le"     );
+    test_transcode!(transcode_utf8_utf16be     , "utf8_th.txt"     , "utf16be_BOM_th.txt"  , "utf-16be"     );
+    test_transcode!(transcode_utf8_sjis        , "utf8_ja.txt"     , "sjis_ja.txt"         , "sjis"         );
+    test_transcode!(transcode_utf8_eucjp       , "utf8_ja.txt"     , "euc-jp_ja.txt"       , "euc-jp"       );
+    test_transcode!(transcode_utf8_iso2022jp   , "utf8_ja.txt"     , "iso-2022-jp_ja.txt"  , "iso-2022-jp"  );
+    test_transcode!(transcode_utf8_big5        , "utf8_zh_CHT.txt" , "big5_zh_CHT.txt"     , "big5"         );
+    test_transcode!(transcode_utf8_gbk         , "utf8_zh_CHS.txt" , "gbk_zh_CHS.txt"      , "gbk"          );
+    test_transcode!(transcode_utf8_gb18030     , "utf8_zh_CHS.txt" , "gb18030_zh_CHS.txt"  , "gb18030"      );
+    test_transcode!(transcode_utf8_euckr       , "utf8_ko.txt"     , "euc-kr_ko.txt"       , "euc-kr"       );
+    test_transcode!(transcode_utf8_koi8r       , "utf8_ru.txt"     , "koi8-r_ru.txt"       , "koi8-r"       );
+    test_transcode!(transcode_utf8_windows1252 , "utf8_es.txt"     , "windows-1252_es.txt" , "windows-1252" );
 }
-
